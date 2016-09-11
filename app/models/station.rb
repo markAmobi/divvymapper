7f46b360b530091a7772a1b21e6369bf1d8b8b6d
Mark Donald Amobi <markdnd007@gmail.com>
2016-09-11 13:28:24 -0500

    use live data to seed stations table

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,2 +1,18 @@
 class Station < ApplicationRecord
+
+  def self.get_live_feed
+    data = HTTP.get("https://feeds.divvybikes.com/stations/stations.json").parse["stationBeanList"]
+    ## convert camel case to snake case.
+    stations = data.map do |s|
+      s.map{|k,v| [k.underscore,v]}.to_h
+    end
+  end
+
+  def self.update_stations
+    self.destroy_all
+    stations = self.get_live_feed
+    stations.each do |station|
+      self.create(station)
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Station.destroy_all
-
-stations_file = File.read("db/stations.json")
-stations = JSON.parse(stations_file)["stationBeanList"]
-
-
-## this converts the Camel case from json to snake case for consistency in ruby.
-stations = stations.map do |s|
-  s.map{|k,v| [k.underscore,v]}.to_h
-end
-
-stations.each do |station|
-  Station.create(station)
-end
+Station.update_stations
