7e003e6eab1acd1971f890826bc6a69d5c6c7be3
Mark Donald Amobi <markdnd007@gmail.com>
2016-09-13 20:56:14 -0500

    change root to stations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'welcome#index'
+  root 'stations#index'
 
   get 'stations', to: 'stations#index'
   get 'stations/:id', to: 'stations#show', as: 'station'
