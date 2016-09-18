18f9804821dd1340a7e3ab1f40ee501eed138288
Mark Donald Amobi <markdnd007@gmail.com>
2016-09-17 22:21:59 -0500

    remove more unnecessary spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'stations#index'
 
   get 'test', to: 'welcome#index'
-
   get 'stations', to: 'stations#index'
   get 'stations/:id', to: 'stations#show', as: 'station'
-
   get 'map', to: 'map#map', as: 'map'
 
 end
