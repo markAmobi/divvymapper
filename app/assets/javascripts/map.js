1d7039e1356d3e8b03a60364b2ae8662c244e081
Mark Donald Amobi <markdnd007@gmail.com>
2016-09-20 06:33:04 -0500

    prevent user from controlling maptype

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,6 +2,7 @@ var map;
 function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.8725614, lng: -87.6245382},
-    zoom: 13
+    zoom: 13,
+    mapTypeControl: false
   });
 }
