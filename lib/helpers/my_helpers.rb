81f06fa4dc0781172acc260887b2aeb8c024f969
Mark Donald Amobi <markdnd007@gmail.com>
2016-09-27 13:27:23 -0500

    first attempt at distance matrix

--- lib/helpers/my_helpers.rb
+++ lib/helpers/my_helpers.rb
@@ -1,3 +1,12 @@
 def do_stuff
   puts "HELLO, there"
 end
+
+# start and end are hashes with lat, lng.
+def get_distance_matrix(start_point, end_point)
+  request_url = ""
+  points = [start_point, end_point].map{|p| p.values.join(",")}.join("|")
+
+  data = HTTP.get(request_url).parse
+
+end
