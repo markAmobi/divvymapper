d20232502295873a11a1c243cc35cd399a2b8a3b
Mark Donald Amobi <markdnd007@gmail.com>
2016-09-28 17:56:23 -0500

    add comment to method

--- lib/helpers/my_helpers.rb
+++ lib/helpers/my_helpers.rb
@@ -2,15 +2,14 @@ def do_stuff
   puts "HELLO, there"
 end
 
+# start and end are {latitude: v, longitude: v}
 def get_distance_matrix(start_point, end_point)
   #sort is just to ensure lat comes first.
   origin = start_point.sort.to_h.values.map(&:to_s).join(",")
   destination = end_point.sort.to_h.values.map(&:to_s).join(",")
 
   request_url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{origin}&destinations=#{destination}&mode=bicycling&key=#{ENV['GOOGLE_MAPS_API_KEY']}"
-
   response = HTTP.get(request_url).parse
-  #creat request url
-  #sent api request
-  #do necessary manipulation
+
+  # still need to do stuff with the response.
 end
