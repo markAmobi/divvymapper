93b2807522d5ddcad50fcc8568f71341203367f8
Mark Donald Amobi <markdnd007@gmail.com>
2016-09-28 17:54:59 -0500

    write method to get distance matrix info, given 2 stations

--- lib/helpers/my_helpers.rb
+++ lib/helpers/my_helpers.rb
@@ -3,6 +3,13 @@ def do_stuff
 end
 
 def get_distance_matrix(start_point, end_point)
+  #sort is just to ensure lat comes first.
+  origin = start_point.sort.to_h.values.map(&:to_s).join(",")
+  destination = end_point.sort.to_h.values.map(&:to_s).join(",")
+
+  request_url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=#{origin}&destinations=#{destination}&mode=bicycling&key=#{ENV['GOOGLE_MAPS_API_KEY']}"
+
+  response = HTTP.get(request_url).parse
   #creat request url
   #sent api request
   #do necessary manipulation
--- lib/tasks/stations_update.rake
+++ lib/tasks/stations_update.rake
@@ -8,3 +8,7 @@ end
 task :test_helper => :environment do
   do_stuff
 end
+
+task :tt => :environment do
+  ap get_distance_matrix({latitude: 41.8725614, longitude: -87.6245382}, {latitude: 41.86722595682,longitude: -87.6153553902})
+end
