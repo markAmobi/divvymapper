f7027b89eb64165838a9ad7f88f2a62a4cc5b7c1
Mark Donald Amobi <markdnd007@gmail.com>
2016-09-30 21:56:12 -0500

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911033427) do
+ActiveRecord::Schema.define(version: 20161001025029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "destinations", force: :cascade do |t|
+    t.integer  "distance"
+    t.string   "distance_text"
+    t.integer  "duration"
+    t.string   "duration_text"
+    t.integer  "origin_id"
+    t.string   "origin_type"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["origin_type", "origin_id"], name: "index_destinations_on_origin_type_and_origin_id", using: :btree
+  end
 
   create_table "stations", force: :cascade do |t|
     t.string   "station_name"
