d39340842aefd71079189f9665fca98944aaae60
Mark Donald Amobi <markdnd007@gmail.com>
2016-09-30 22:20:13 -0500

    add methods to get distance matrix among stations

--- app/models/station.rb
+++ app/models/station.rb
@@ -17,7 +17,30 @@ class Station < ApplicationRecord
     end
   end
 
+
+  def update_destination(other_station)
+    dest_info = self.get_distance_info(other_station)
+    d = Destination.new
+    d.assign_attributes(dest_info)
+    d.origin = self
+    d.save!
+  end
+
+  ##for updating individual stations destinations.
+  def update_destinations
+    Station.find_each do |station|
+      update_destination(other_station)
+    end
+  end
+
+  ## this method will be used to update the destinations among all stations.
+  def self.update_all_destinations
+    Station.find_each do |station|
+      station.update_destinations
+    end
+  end
+
   def get_distance_info(other_station)
-    get_distance_matrix(location, other_station.location)
+    parse_distance_matrix(get_distance_matrix(location, other_station.location))
   end
 end
