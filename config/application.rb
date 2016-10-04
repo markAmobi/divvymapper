99fa6bc1ff92ded8fa3ae9c6a5b768b3a1de968c
Mark Donald Amobi <markdnd007@gmail.com>
2016-10-03 20:44:57 -0500

    remove loading of yml file from before config block

--- config/application.rb
+++ config/application.rb
@@ -12,11 +12,11 @@ module Divvymapper
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
     end
+    env_file = File.join(Rails.root, 'config', 'local_env.yml')
+    YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(env_file)
 
     ## make lib/helpers files available. (I need this for my rake tasks. )
     Dir[Rails.root.join('lib/helpers/*.rb')].each {|file| require file }
