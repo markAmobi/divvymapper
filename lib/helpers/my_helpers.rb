c0cbc7142f32479c484108c9e1c0b3620203ec4a
Mark Donald Amobi <markdnd007@gmail.com>
2016-10-05 18:55:14 -0500

    ad method to translate distance matrix response to be easier to store in my database

--- lib/helpers/my_helpers.rb
+++ lib/helpers/my_helpers.rb
@@ -15,6 +15,36 @@ def get_distance_duration(start_locations, end_locations)
   response = HTTP.get(request_url).parse
 end
 
+## transform google API response to what I feel will be easier for me to handle.
+def translate_response(start_locations, end_locations, response)
+
+  final_output = {}
+  o_addresses = response["origin_addresses"]
+  d_addresses = response["destination_addresses"]
+  rows = response["rows"]
+
+  start_locations.each_with_index do |location, o_index|
+
+    dest_hash = {"origin_address" => o_addresses[o_index]}
+
+    rows[o_index]["elements"].each_with_index do |destination_details, d_index|
+      d = {}
+      d["distance"] = destination_details["distance"]["value"]
+      d["distance_text"] = destination_details["distance"]["text"]
+      d["duration"] = destination_details["duration"]["value"]
+      d["duration_text"] = destination_details["duration"]["text"]
+      d["address"] = d_addresses[d_index]
+
+      dest_hash[end_locations[d_index]] = d
+    end
+
+
+
+    final_output[location] = dest_hash
+  end
+
+  final_output
+end
 
 ##assume single origin-destination.
 ##this is used to parse response of call to distance matrix api. get_distance_matrix
