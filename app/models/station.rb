d96802beb5778a080ea4a483bc1dc09bddb0b32e
Mark Donald Amobi <markdnd007@gmail.com>
2016-10-05 20:19:13 -0500

    update comment

--- app/models/station.rb
+++ app/models/station.rb
@@ -26,7 +26,7 @@ class Station < ApplicationRecord
   end
 
 
-  ## use result of parsed_response to update stations destionations.
+  ## use result of parsed_response to update stations destinations to database.
   def self.update_station_details(parsed_response)
     parsed_response.each do |o_coords, o_details|
       start_station = Station.where(geo_coords: "{#{o_coords.join(',')}}").first
@@ -50,48 +50,4 @@ class Station < ApplicationRecord
     end
   end
 
-
-  #
-  # def update_destination(other_station)
-  #   dest_info = self.get_distance_info(other_station)
-  #   d = Destination.new
-  #   d.assign_attributes(dest_info)
-  #   d.origin = self
-  #   d.save!
-  # end
-
-  ##for updating individual stations destinations.
-  # def update_destinations
-    ## see https://developers.google.com/maps/documentation/distance-matrix/usage-limits
-    ## really need a way send multiple requests while still within API rate limits.
-    ## right now, I'm doing 1pair of origin/destination per request.
-    ## need 580 * 580 = 336400 based on my current setup.
-    ## can cut this in half by adding some logic not to redo pairs that have been done.
-    ## this is still a lot. 168200.
-    ## even if I get 25 destination per request, that still leaves me with 6728 requests.
-    ## but I have 2500 per day. I should be able to set this up so I just get all the data I need
-    ## in three days. and probably store in csv or something so I don't have to get it again.
-    ## before going ahead to implement this, I should probably think about interaction with the directions
-    ## API and see if it's independent. if not, then i have to do that at same time in order to have data to
-    ## to display directions info on the map.
-    ## need to look into encoded polyline, maybe? or just put multiple destinations in request.
-    # raise "DONT DO THIS!!!! Google API limits has limits. "
-    # Station.find_each do |station|
-    #   update_destination(station)
-    # end
-  # end
-
-  ## this method will be used to update the destinations among all stations.
-  # def self.update_all_destinations
-  #
-  #   ## due to ap usage limits, this method might use different keys if run once.
-  #
-  #   Station.find_each do |station|
-  #     station.update_destinations
-  #   end
-  # end
-
-  # def get_distance_info(other_station)
-  #   parse_distance_matrix(get_distance_matrix(location, other_station.location))
-  # end
 end
