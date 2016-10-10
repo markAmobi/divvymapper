42c06dbd1840c593e2ee4ed7783e37811f891baa
Mark Donald Amobi <markdnd007@gmail.com>
2016-10-10 17:06:45 -0500

    comment out destination table migration

--- db/migrate/20161001025029_create_destinations.rb
+++ db/migrate/20161001025029_create_destinations.rb
@@ -1,19 +1,19 @@
-class CreateDestinations < ActiveRecord::Migration[5.0]
-  def change
-    create_table :destinations do |t|
-      t.integer :distance
-      t.string :distance_text
-      t.integer :duration
-      t.string :duration_text
-      t.integer :origin_id
-      t.string :origin_type
-      t.string :origin_address
-      t.string :address
-      t.float :geo_coords, array: true, default: []
-      
-      t.timestamps
-    end
-
-    add_index :destinations, [:origin_type, :origin_id]
-  end
-end
+# class CreateDestinations < ActiveRecord::Migration[5.0]
+#   def change
+#     create_table :destinations do |t|
+#       t.integer :distance
+#       t.string :distance_text
+#       t.integer :duration
+#       t.string :duration_text
+#       t.integer :origin_id
+#       t.string :origin_type
+#       t.string :origin_address
+#       t.string :address
+#       t.float :geo_coords, array: true, default: []
+#
+#       t.timestamps
+#     end
+#
+#     add_index :destinations, [:origin_type, :origin_id]
+#   end
+# end
