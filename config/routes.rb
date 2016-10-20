4e1258e0f95a87e9da7b2938ec49a54c93837f33
Mark Donald Amobi <markdamobi@gmail.com>
2016-10-19 20:02:33 -0500

    change landing page to maps

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'stations#index'
+  # root 'stations#index'
+  root 'map#map'
 
   get 'test', to: 'welcome#index'
   get 'stations', to: 'stations#index'
